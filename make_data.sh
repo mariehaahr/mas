267b23323796f074f1e3c18f252f198eab7ea55b
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2025-11-13 09:48:39 +0100

    ready to run all

--- make_data.sh
+++ make_data.sh
@@ -12,9 +12,9 @@ echo "Host: $(hostname)"
 set -euo pipefail
 
 
-uv run src/prepare_round2.py mistral-0.3-7b
-uv run src/prepare_round2.py llama-3.2-1b
-uv run src/prepare_round2.py llama-3.2-3b
+# uv run src/prepare_round2.py mistral-0.3-7b
+# uv run src/prepare_round2.py llama-3.2-1b
+# uv run src/prepare_round2.py llama-3.2-3b
 uv run src/prepare_round2.py qwen-2.5-7b
-uv run src/prepare_round2.py qwen-2.5-1.5b
-uv run src/prepare_round2.py mistral-0.2-7b
+# uv run src/prepare_round2.py qwen-2.5-1.5b
+# uv run src/prepare_round2.py mistral-0.2-7b
