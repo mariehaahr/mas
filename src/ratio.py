1a50d093825f9e64483b5b4d3bf31b93f05bf160
fridanl <frida.nl@hotmail.com>
2025-11-19 15:21:44 +0100

    code for ratio df round 2

--- src/ratio.py
+++ src/ratio.py
@@ -1,7 +1,13 @@
 import pathlib, yaml
 import pandas as pd 
+import argparse
 
-def main():
+def make_ratio(round_num: int):
+
+    if round_num not in (1, 2):
+        raise ValueError(f'round_num must be either 1 or 2, got {round_num}')
+    
+
     profiles_root = yaml.safe_load(pathlib.Path('configs/models.yaml').read_text())
     profiles = profiles_root.get('profiles', {})
     model_names = profiles.keys()
@@ -9,7 +15,10 @@ def main():
     dfs = []
     # read in all results
     for m_name in model_names:
-        p = f'/home/rp-fril-mhpe/first-{m_name}.csv'
+        if round == 1: 
+            p = f'/home/rp-fril-mhpe/first-{m_name}.csv'
+        else:
+            p = f'/home/rp-fril-mhpe/second-{m_name}.csv'
 
         try:
             df = pd.read_csv(p)
@@ -21,19 +30,46 @@ def main():
 
     combined = pd.concat(dfs, ignore_index=True)
 
-    # group by model, id
+    if round_num == 1:
+        group_cols = ['model', 'id']
+        label_col = 'label'
+        save_as = 'first-results-sarc-ratio.csv'
+
+    else:
+        group_cols = ['model_sender', 'id', 'model_receiver']
+        label_col = 'label_receiver_new'
+        save_as = 'second-results-sarc-ratio.csv'
+
+    # group by cols
     aggregated = (
-    combined.groupby(['model', 'id'], as_index=False)
+    combined.groupby(group_cols, as_index=False)
     .agg(
-        sarc_count=('label', lambda x: (x == 'sarcastic').sum()),
+        sarc_count=(label_col, lambda x: (x == 'sarcastic').sum()),
         valid_json_count=('valid_json', lambda x: (x == True).sum()),
         )
     )
-    aggregated['sarc_ratio'] = aggregated['sarc_count'] / aggregated['valid_json_count']
+    
+    aggregated['sarc_ratio'] = aggregated['sarc_count'] / aggregated['valid_json_count'].replace({0: pd.NA})
+
     aggregated.drop('sarc_count', inplace=True, axis = 1)
-    print(aggregated['model'].unique())
+
+    out_path = f'/home/rp-fril-mhpe/{save_as}'
     # save 
-    aggregated.to_csv('/home/rp-fril-mhpe/results-sarc-ratio.csv', index=False)
+    aggregated.to_csv(out_path, index=False)
+    print(f'Saved ratios to {out_path}')
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Compute sarcastic ratios for round 1 and round 2')
+
+    parser.add_argument(
+        '--round',
+        type=int,
+        required=True,
+        choices=[1,2],
+        help='Which round to compute results for (1 or 2).',
+    )
+
+    args = parser.parse_args()
+    
+    make_ratio(args.round)
