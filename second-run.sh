045fb812af133c0c68e0f59c3d624195d9155c1f
fridanl <frida.nl@hotmail.com>
2025-11-19 14:54:12 +0100

    plots

--- second-run.sh
+++ second-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=mistral-0.3-7b-2ndrun
+#SBATCH --job-name=qwen-2.5-1.5b-2ndrun
 #SBATCH --partition=acltr
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=20:00:00
+#SBATCH --time=14:00:00
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --error=logs/%x.%j.err
 #SBATCH --constraint="gpu_h100|gpu_a100_80gb"
@@ -21,5 +21,7 @@ set -euo pipefail
 
 uv sync 
 
-uv run run-eval-round2.py --model_name mistral-0.3-7b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_mistral-0.3-7b.csv
+#uv run run-eval-round2.py --model_name mistral-0.3-7b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_mistral-0.3-7b.csv
 
+
+uv run run-eval-round2.py --model_name qwen-2.5-1.5b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_qwen-2.5-1.5b.csv
