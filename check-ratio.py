2f6b5a40ae9d0d621ed5a8705915228c9d955a37
fridanl <frida.nl@hotmail.com>
2025-11-19 16:23:14 +0100

    changes to ratio

---
+++ check-ratio.py
@@ -0,0 +1,19 @@
+
+import pandas as pd 
+
+
+df1 = pd.read_csv('/home/rp-fril-mhpe/results-sarc-ratio.csv')
+df1 = pd.read_csv('/home/rp-fril-mhpe/first-results-sarc-ratio.csv')
+
+
+df1_norm = df1.copy()
+df2_norm = df2.copy()
+
+# Replace NaN with 0 only in valid_json column
+df1_norm['valid_json'] = df1_norm['valid_json'].fillna(0)
+df2_norm['valid_json'] = df2_norm['valid_json'].fillna(0)
+
+# Now compare
+same = df1_norm.equals(df2_norm)
+print("Same?", same)
+
--- make-ratio-df.sh
+++ make-ratio-df.sh
@@ -4,13 +4,14 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
 #SBATCH --time=00:20:00
-#SBATCH --output=logs/ratio/%x.%j.out
-#SBATCH --error=logs/ratio/%x.%j.err
+#SBATCH --output=logs/ratio2/%x.%j.out
+#SBATCH --error=logs/ratio2/%x.%j.err
 
 
 echo "Host: $(hostname)"
 
 set -euo pipefail
-
+uv sync
 uv run src/ratio.py --round 1
 uv run src/ratio.py --round 2
+uv run check-ratio.py
