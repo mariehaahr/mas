552cd01689d2dac6766cf9fe8f49c81c5940d10d
Marie Haahr Petersen <marie.haahr.petersen@gmail.com>
2025-11-20 09:52:25 +0100

    ready to run mistral-0.3 ready for round 2 again, after fixing bug

--- second-run.sh
+++ second-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=mistral-0.2-7b-2ndrun
+#SBATCH --job-name=mistral-0.3-7b-2ndrun
 #SBATCH --partition=acltr
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
@@ -23,4 +23,4 @@ uv sync
 
 #uv run run-eval-round2.py --model_name mistral-0.3-7b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_mistral-0.3-7b.csv
 #uv run run-eval-round2.py --model_name qwen-2.5-1.5b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_qwen-2.5-1.5b.csv
-uv run run-eval-round2.py --model_name mistral-0.2-7b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_mistral-0.2-7b.csv
+uv run run-eval-round2.py --model_name mistral-0.3-7b --outdir /home/rp-fril-mhpe --dataset_path /home/rp-fril-mhpe/input_mistral-0.3-7b.csv
