5e1956af7940a5dba0fa7425c59b4291b4faf2a3
fridanl <frida.nl@hotmail.com>
2025-11-29 11:14:16 +0100

    changed structure a bit

--- first-run.sh
+++ first-run.sh
@@ -3,7 +3,6 @@
 #SBATCH --partition=acltr
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
 #SBATCH --time=25:00:00
 #SBATCH --output=logs/%x.%j.out
 #SBATCH --error=logs/%x.%j.err
